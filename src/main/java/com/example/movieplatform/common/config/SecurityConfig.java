661cdddda76359c6b3490d883ba3c4d2c96aa053
{ehdgur123} <{kingdong710@naver.com}>
2025-09-12 00:44:53 +0900

    [feat]: 로그인 타임리프에서 리액트로 변경

--- src/main/java/com/example/movieplatform/common/config/SecurityConfig.java
+++ src/main/java/com/example/movieplatform/common/config/SecurityConfig.java
@@ -1,15 +1,21 @@
 package com.example.movieplatform.common.config;
 
 import com.example.movieplatform.auth.service.CustomOAuth2UserService;
+import com.example.movieplatform.auth.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +23,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -27,6 +38,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
          http
+                 .csrf(AbstractHttpConfigurer::disable)
+                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                  .authorizeHttpRequests((auth) -> auth
                          .requestMatchers( "/","/login", "/register").permitAll()
                          .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
@@ -35,11 +48,8 @@ public class SecurityConfig {
                          .anyRequest().authenticated()
                  )
 
-                 .formLogin((auth) -> auth
-                         .loginPage("/login")
-                         .defaultSuccessUrl("/", true)
-                         .permitAll()
-                 )
+                 .formLogin(AbstractHttpConfigurer::disable)
+                 .httpBasic(AbstractHttpConfigurer::disable)
 
                  .oauth2Login((oauth2)-> oauth2
                          .loginPage(("/login"))
@@ -53,5 +63,18 @@ public class SecurityConfig {
                  .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);  // 쿠키 허용
+        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
