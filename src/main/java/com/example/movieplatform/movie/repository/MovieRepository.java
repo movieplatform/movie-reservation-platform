df930309ba47f8ea6efea712626ab1fa0a614a06
{ehdgur123} <{kingdong710@naver.com}>
2025-09-14 23:01:45 +0900

    [feat]: 영화 장르별 페이징 처리(아직 미완성)

--- src/main/java/com/example/movieplatform/movie/repository/MovieRepository.java
+++ src/main/java/com/example/movieplatform/movie/repository/MovieRepository.java
@@ -1,7 +1,11 @@
 package com.example.movieplatform.movie.repository;
 
 import com.example.movieplatform.movie.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +15,12 @@ public interface MovieRepository extends JpaRepository<Movie, String> {
     List<Movie> findAllByOrderByRepRlsDateDesc();
     // 개봉일 기준 내림차순으로 상위 10개 영화 조회
     List<Movie> findTop10ByOrderByRepRlsDateDesc();
+    // 장르별 영화 조회 (페이징)
+    @Query("SELECT DISTINCT m FROM MovieGenre mg " +
+            "JOIN mg.movie m " +
+            "JOIN mg.genre g " +
+            "WHERE g.name = :genreName " +
+            "ORDER BY m.title ASC")  // Movie 기준 정렬
+    Page<Movie> findByGenreName(@Param("genreName") String genreName, Pageable pageable);
+
 }
