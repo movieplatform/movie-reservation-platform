fdecdc7115c84271b91913b6f5be3a76f36e9729
{ehdgur123} <{kingdong710@naver.com}>
2025-10-18 16:19:46 +0900

    [feat]: 배포 연습

--- src/main/java/com/example/movieplatform/HomeController.java
+++ src/main/java/com/example/movieplatform/HomeController.java
@@ -23,7 +23,7 @@ public class HomeController {
 
         Movie dummy = new Movie();
         dummy.setDocId("123");
-        dummy.setTitle("Dummy Test Movie11");
+        dummy.setTitle("Dummy Test Movie111");
         dummy.setRating("3.0");
         movies.add(dummy);
 
